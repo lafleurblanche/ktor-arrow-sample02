4719ec604337534e16852f4cd38e40992fb9261b
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-09 20:57:49 +0900

    20221009 定数化

--- src/main/kotlin/net/konohana/sakuya/arrow/sample02/plugins/Routing.kt
+++ src/main/kotlin/net/konohana/sakuya/arrow/sample02/plugins/Routing.kt
@@ -9,12 +9,13 @@ import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import net.konohana.sakuya.arrow.sample02.constant.ErrorMessageConst
 import net.konohana.sakuya.arrow.sample02.exception.CodeMismatchException
 
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<CodeMismatchException> { call, _ ->
-            call.respond(HttpStatusCode.BadRequest, mapOf("result" to "コード形式誤り"))
+            call.respond(HttpStatusCode.BadRequest, mapOf("result" to ErrorMessageConst.ERROR_MSG_CODE_MISMATCH))
         }
     }
 
