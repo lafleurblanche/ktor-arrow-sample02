4c9eb51dcd25ff78cca6abf773d16132d5ca2ac1
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-09 18:45:21 +0900

    20221009 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/arrow/sample02/constant/FromStaCodePatternConst.kt
@@ -0,0 +1,10 @@
+package net.konohana.sakuya.arrow.sample02.constant
+
+class FromStaCodePatternConst {
+    companion object {
+        /** 乗車駅コードパターン: Cerisier */
+        const val CODE_PATTERN_CERISIER = "FRCE"
+        /** 乗車駅コードパターン: NeueGATE */
+        const val CODE_PATTERN_NEUEGATE = "FRGA"
+    }
+}
