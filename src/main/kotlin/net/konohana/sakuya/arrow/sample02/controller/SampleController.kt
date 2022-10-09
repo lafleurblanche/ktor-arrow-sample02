d2e53fddf4f67daa64cfed00eb3c38d4d0fcf551
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-09 20:57:44 +0900

    20221009 定数化

--- src/main/kotlin/net/konohana/sakuya/arrow/sample02/controller/SampleController.kt
+++ src/main/kotlin/net/konohana/sakuya/arrow/sample02/controller/SampleController.kt
@@ -6,6 +6,7 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import net.konohana.sakuya.arrow.sample02.constant.ErrorCodeConst
+import net.konohana.sakuya.arrow.sample02.constant.ErrorMessageConst
 import net.konohana.sakuya.arrow.sample02.exception.CodeMismatchException
 import net.konohana.sakuya.arrow.sample02.utils.check
 import net.konohana.sakuya.arrow.sample02.utils.checkNumber
@@ -18,11 +19,11 @@ fun Route.sampleController() {
             val rst = check(data)
             println(rst)
             if (rst == ErrorCodeConst.ERROR_101) {
-                throw CodeMismatchException("コード形式誤り")
+                throw CodeMismatchException(ErrorMessageConst.ERROR_MSG_CODE_MISMATCH)
             }
             val rst2 = checkNumber(rst)
             if (rst2 == ErrorCodeConst.ERROR_102) {
-                throw CodeMismatchException("コード形式誤り")
+                throw CodeMismatchException(ErrorMessageConst.ERROR_MSG_CODE_MISMATCH)
             }
             call.respond(mapOf("result" to rst2))
         }
