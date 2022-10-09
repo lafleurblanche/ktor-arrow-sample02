b5c169a65206acba5639c8151cfd81adcb87f310
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-10-09 20:57:57 +0900

    20221009 NEW

---
+++ src/main/kotlin/net/konohana/sakuya/arrow/sample02/constant/ErrorMessageConst.kt
@@ -0,0 +1,12 @@
+package net.konohana.sakuya.arrow.sample02.constant
+
+/**
+ * エラーコード定数クラス
+ * @author lafleurblanche
+ */
+class ErrorMessageConst {
+    companion object {
+        const val ERROR_MSG_CODE_MISMATCH = "コード形式誤り"
+        const val ERROR_MSG_CODE_NOTFOUND = "コードなし"
+    }
+}
